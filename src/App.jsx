00928e8ef93be71bc8a1470fa16fa3db1dd19c6d
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-07-17 23:09:19 -0400

    feat: Create mobile navigation
    - Add layout and styling and functionality for mobile nav
    - Style added to html element min-width
    - Add classes in tailwind.css file for mobile nav animation and hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Header from '@/components/Header'
 
+
+
 function App() {
   
   return (
   <div>
-    <h2>test</h2>
     <Header/>
   </div> 
   )
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,44 @@
-
+import { useState } from "react"
 
 const Header = () => {
+  const [isMenuToggled, setIsMenuToggled] = useState(false)
+
+function handleToggleMobileMenu() {
+  setIsMenuToggled((prevState => !prevState))
+}
+
+
+ const updateMobileMenuIcon = isMenuToggled ? <svg className="w-10" xmlns="http://www.w3.org/2000/svg"     fill="none" viewBox="0 0 24 24" stroke-width="1.5"      stroke="currentColor" class="w-6 h-6">
+<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+</svg> : <svg className="w-10"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" /> </svg>
+
+
   return (
-    <header>
-      <h4>header test</h4>
+    <>
+    <header className=" fixed w-full bg-neutral-50 shadow-md ">
+          <div className=" relative flex z-50  bg-neutral-50 justify-between items-center p-3 w-full ">
+              <div>
+                <img src="#" alt="" />
+                <h4 className="text-lg font-bold text-lime-600 ">Bloko.io</h4>
+              </div>
+                  <button onClick={handleToggleMobileMenu}
+                  className="text-neutral-900" >
+                    {updateMobileMenuIcon}
+                  </button>
+              </div> 
+             {/* <div className={isMenuToggled} > */}
+           <div className={` absolute z-0  w-full bg-neutral-900  p text-neutral-50 ${isMenuToggled ? 'mobile-menu-hidden': 'mobile-menu-visible'  } `} >
+                    <nav>
+                      <ul className=" nav-hover-item flex flex-col text-center text-lg ">
+                          <li className=" p-3 " ><a href="#">Explore</a></li>
+                          <li className="p-3 border-y-2 border-zinc-700" ><a href="#">News</a></li>
+                          <li className="p-3" ><a href="#">Learn more</a></li>
+                      </ul>
+                    </nav>
+                </div> 
     </header>
+    </>
   )
 }
 
